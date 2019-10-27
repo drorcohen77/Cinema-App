2062230f6cd4b744161d7e582d21dc0a9bc4e32d
drshualy <drorc77@gmail.com>
2019-10-27 21:35:41 +0200

    favorites guard

--- server.js
+++ server.js
@@ -6,16 +6,16 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the angularapp directory
-// app.use(express.static(__dirname + '/dist/cinemapicker'));
-app.use('/', express.static('cinemapicker', { redirect: false }));
+app.use(express.static(__dirname + '/dist/cinemapicker'));
+// app.use('/', express.static('cinemapicker', { redirect: false }));
 
-// app.get('/main/movies', function(req, res) {
+app.get('*', function(req, res) {
 
-//     res.sendFile(path.join(__dirname + '/dist/cinemapicker/index.html'));
-// });
-app.get('*', function(req, res, next) {
-    res.sendFile(path.resolve('cinemapicker/index.html'));
+    res.sendFile(path.join(__dirname + '/dist/cinemapicker/index.html'));
 });
+// app.get('*', function(req, res, next) {
+//     res.sendFile(path.resolve('cinemapicker/index.html'));
+// });
 
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
